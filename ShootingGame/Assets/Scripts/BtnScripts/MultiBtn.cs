7149359c0773837500596a307308c8afb75cfcf5
gpjj <iwbgp019@naver.com>
2024-05-20 17:10:46 +0900

    feat : PlayerChoiceScene 구현

---
+++ ShootingGame/Assets/Scripts/BtnScripts/MultiBtn.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MultiBtn : MonoBehaviour
+{
+    public void Multi()
+    {
+        SceneManager.LoadScene("MultiScene");
+    }
+}
