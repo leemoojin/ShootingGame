dd85e32e56fc398ddc0eae6570e8c2b6ce969342
gpjj <iwbgp019@naver.com>
2024-05-21 19:55:39 +0900

    feat : bomb ui

--- ShootingGame/Assets/Scripts/Player/Attack/BombAmountController.cs
+++ ShootingGame/Assets/Scripts/Player/Attack/BombAmountController.cs
@@ -1,9 +1,11 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BombAmountController : MonoBehaviour
 {
     public int currentBombCount;
     [SerializeField] private int maximumBombCount;
+    public Image[] bombImages; // 폭탄 이미지 배열
 
     public void UseBomb(int bombCount)
     {
@@ -31,5 +33,30 @@ public class BombAmountController : MonoBehaviour
         {
             currentBombCount = maximumBombCount;
         }
+    }
+    private void Start()
+    {
+        UpdateBombImages(); // 초기화할 때 UI 이미지 업데이트
+    }
+    private void UpdateBombImages()
+    {
+        for (int i = 0; i < maximumBombCount; i++)
+        {
+            // 현재 폭탄 갯수에 따라 이미지 활성화/비활성화
+            if (i < currentBombCount)
+            {
+                bombImages[i].enabled = true; // 활성화
+            }
+            else
+            {
+                bombImages[i].enabled = false; // 비활성화
+            }
+        }
+    }
+    // 폭탄 갯수 변경 메서드
+    public void SetBombCount(int bombCount)
+    {
+        currentBombCount = bombCount;
+        UpdateBombImages(); // 변경 후 UI 이미지 업데이트
     }
 }
